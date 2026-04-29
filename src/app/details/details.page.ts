6dbf57ecab89bd1f562758b6dd1f19716b4159f3
Konstantin <nilovk@gmail.com>
2026-04-29 14:19:02 +0100

    Favourites, product details page (.ts, .html)

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,19 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonItem, IonLabel, IonIcon, IonButtons } from '@ionic/angular/standalone';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { addIcons } from 'ionicons';
+import { home, heart } from 'ionicons/icons';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonLabel, IonList, IonItem, IonIcon, IonButtons, RouterLink]
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  person: any;
+  movies: any[] = [];
+
+  constructor(private route: ActivatedRoute) {
+    addIcons({ home, heart });
   }
 
+  ngOnInit() {
+
+    var id = this.route.snapshot.paramMap.get('id');
+
+    // Person details
+    fetch('https://api.themoviedb.org/3/person/' + id + '?api_key=' + environment.tmdbApiKey)
+      .then(res => res.json())
+      .then(data => {
+        this.person = data;
+      });
+
+    // Known for movies
+    fetch('https://api.themoviedb.org/3/person/' + id + '/movie_credits?api_key=' + environment.tmdbApiKey)
+      .then(res => res.json())
+      .then(data => {
+        this.movies = (data.cast || []).slice(0, 5);
+      });
+  }
+
+  getImage(path: string) {
+    return 'https://image.tmdb.org/t/p/w200' + path;
+  }
 }
--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,19 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router, RouterLink } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonItem, IonList, IonLabel, IonIcon  } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { home, heart } from 'ionicons/icons';
+
+
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonButtons, IonItem, IonIcon, IonList, 
+    RouterLink, CommonModule, IonLabel, ]
 })
 export class FavouritesPage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  movies: any[] = [];
+
+  constructor(private router: Router) {
+    addIcons({ home, heart });
   }
 
+  ngOnInit() {
+    this.loadFavourites();
+  }
+
+  ionViewWillEnter() {
+    this.loadFavourites();
+  }
+
+  loadFavourites() {
+    var data = localStorage.getItem('favourites');
+    this.movies = data ? JSON.parse(data) : [];
+  }
+
+  getImage(path: string) {
+    return 'https://image.tmdb.org/t/p/w200' + path;
+  }
+
+  openDetails(id: number) {
+    this.router.navigate(['/movie-details', id]);
+  }
 }
